afd2da9b2789ecef031f23e2d0d024051655570f
Vonglory176 <SkylerGConley@gmail.com>
2023-10-22 16:25:46 -0700

    Initial skeleton frame of the Navbar component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import netflixLogo from "../assets/images/netflixLogo.jsx"
+
+export default function Header() {
+    <section id="header-section">
+        {netflixLogo}
+        <div>
+            
+        </div>
+    </section>
+}
