c3baeed25a07d47e84b17cf9d59d4866b7675e23
Vonglory176 <SkylerGConley@gmail.com>
2023-10-28 16:41:34 -0700

    Changes to incorperate new EmailSignUp component

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,6 +1,5 @@
-import { Button, Form } from "react-bootstrap";
+import EmailSignUp from "./EmailSignUp";
 import BackgroundSmall from "../assets/images/US-en-20231016-popsignuptwoweeks-perspective_alpha_website_small.jpg";
-import RightArrow from "../assets/images/RightArrow.svg"
 
 export default function Landing() {
     return (
@@ -14,18 +13,7 @@ export default function Landing() {
             <h1>Unlimited movies, TV shows, and more</h1>
             <p>Watch anywhere. Cancel anytime.</p>
 
-            <div className="emailCallToAction-div">
-                <p>Ready to watch? Enter your email to create or restart your membership.</p>
-                <Form data-bs-theme="dark">
-                    <Form.Group className="" controlId="EmailSignUp">
-                        <Form.Control type="email" placeholder="Email address" required/>
-                    </Form.Group>
-                    <Button type="submit" size="lg">
-                        Get Started
-                        <img src={RightArrow}/>
-                    </Button>
-                </Form>
-            </div>
+            <EmailSignUp/>
         </main>
     </section>
     )
