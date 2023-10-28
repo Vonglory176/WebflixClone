600a44b4b5a041bb9c6814489773e86f1642ab30
Vonglory176 <SkylerGConley@gmail.com>
2023-10-28 16:37:23 -0700

    EmailSignup given a component for duplicate use

---
+++ src/components/EmailSignUp.jsx
@@ -0,0 +1,19 @@
+import { Button, Form } from "react-bootstrap";
+import RightArrow from "../assets/images/RightArrow.svg"
+
+export default function EmailSignUp() {
+    return (
+    <div className="emailCallToAction-div">
+        <Form data-bs-theme="dark">
+        <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+            <Form.Group className="" controlId="EmailSignUp">
+                <Form.Control type="email" placeholder="Email address" required/>
+            </Form.Group>
+            <Button type="submit" size="lg">
+                Get Started
+                <img src={RightArrow}/>
+            </Button>
+        </Form>
+    </div>
+    )
+}
