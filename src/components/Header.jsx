a19e78461138433991c8c2afbba93364082e734a
Vonglory176 <SkylerGConley@gmail.com>
2023-10-28 16:40:53 -0700

    Changes to incorperate new EmailSignUp component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,14 @@
-import { Button, Form } from "react-bootstrap";
 import NetflixLogo from "../assets/images/netflixLogo.svg"
-import LanguageIcon from "../assets/images/LanguageIcon.svg"
+import LanguageSelector from "./LanguageSelector";
+import { Button } from "react-bootstrap";
 
 export default function Header() {
     return (
     <section id="header-section">
         <header>
             <img src={NetflixLogo} alt="Netflix Logo"/>
-            <div className="languageSelector-div">
-                <img src={LanguageIcon} alt="Language Icon"/>
-                <Form.Select aria-label="Language Selector" size="sm" data-bs-theme="dark">
-                    <option>English</option>
-                    <option>Español</option>
-                </Form.Select>
+            <div className="buttonGroup-div">
+                <LanguageSelector/>
                 <Button variant="danger" size="sm">Sign In</Button>
             </div>
         </header>
