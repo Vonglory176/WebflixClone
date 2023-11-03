f557cca998f594bd6464f7793b9286899dbe5995
Vonglory176 <SkylerGConley@gmail.com>
2023-11-03 16:13:07 -0700

    More ToDo commentation

--- src/App.jsx
+++ src/App.jsx
@@ -21,11 +21,15 @@ function App() {
 export default App
 
 /*
+Major breakpoints at: 600px, 960px, 1280px, 1920px
+
 TODO
 -------------------
 Now!
 ------
-Start expanding into the next viewport (sm-md)
+Start expanding into the next viewport (600px-959px)
+Create second view for AdBanner at screen width 655px
+Check for more media queries between 655px and 959px
 
 Later!
 ------
@@ -34,5 +38,12 @@ Email input needs an animated label
 SVG coloring for arrows, plus and other need to be fixed
 Weird border coloring issue with accordions
 Language selector needs tweaks, specifically its dropdown (placement/colors)
+
+Check image margin/padding in "Download" feature section 
+Check landing section gradient media queries
+
+ALT tags on all images!
+Check Aria mumbo jumbo
+Run an HTML checker
 -------------------
 */
