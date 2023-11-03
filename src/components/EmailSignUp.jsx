fb8a0e7a74dd6646eb0d4076477de7d1d2314c2f
Vonglory176 <SkylerGConley@gmail.com>
2023-11-03 16:10:35 -0700

    Moved h3 element outside form

--- src/components/EmailSignUp.jsx
+++ src/components/EmailSignUp.jsx
@@ -4,8 +4,8 @@ import RightArrow from "../assets/images/RightArrow.svg"
 export default function EmailSignUp() {
     return (
     <div className="emailSignUp-div">
-        <Form data-bs-theme="dark">
         <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+        <Form data-bs-theme="dark">
             <Form.Group className="" controlId="EmailSignUp">
                 <Form.Control type="email" placeholder="Email address" required/>
             </Form.Group>
