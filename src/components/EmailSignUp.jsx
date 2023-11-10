00ebc9c39c9a0053f1a83407e459b6ff772799f9
Vonglory176 <SkylerGConley@gmail.com>
2023-11-10 15:26:36 -0800

    Setup for email label animation

--- src/components/EmailSignUp.jsx
+++ src/components/EmailSignUp.jsx
@@ -7,8 +7,9 @@ export default function EmailSignUp() {
     <div className="emailSignUp-div">
         <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
         <Form data-bs-theme="dark">
-            <Form.Group className="" controlId="EmailSignUp">
-                <Form.Control type="email" placeholder="Email address" required/>
+            <Form.Group className="inputWrapper" controlId="EmailSignUp">
+                {/* <Form.Label>Email address</Form.Label> */}
+                <Form.Control type="email" required/> {/*placeholder="Email address"*/}
             </Form.Group>
             <Button type="submit" size="lg">
                 Get Started
