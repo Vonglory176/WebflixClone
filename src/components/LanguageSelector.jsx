1439ce4edc9878902a66fe10329be0594686f4c7
Vonglory176 <SkylerGConley@gmail.com>
2023-11-10 15:27:49 -0800

    Form data-theme changed from dark to light

--- src/components/LanguageSelector.jsx
+++ src/components/LanguageSelector.jsx
@@ -3,9 +3,9 @@ import { Button, Form } from "react-bootstrap";
 
 export default function LanguageSelector() {
     return (
-    <div className="languageSelector-div">
+    <div className="languageSelector-div inputWrapper">
         {/* <img src={LanguageIcon} alt="Language Icon"/> */}
-        <Form.Select aria-label="Language Selector" size="sm" data-bs-theme="dark">
+        <Form.Select aria-label="Language Selector" size="sm" data-bs-theme="light">
             <option>English</option>
             <option>Español</option>
         </Form.Select>
