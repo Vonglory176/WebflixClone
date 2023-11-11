418d3d4fe3182ecfa4ae997a22078ee357f7908c
Vonglory176 <SkylerGConley@gmail.com>
2023-11-11 10:46:36 -0800

    Simple copyright banner with styling

---
+++ src/components/CopyrightBanner.jsx
@@ -0,0 +1,7 @@
+export default function CopyrightBanner() {
+    return (
+        <section className="copyrightBanner-section" style={{width:"100%", backgroundColor: "rgb(35, 35, 35)"}}>
+            <h6 style={{textAlign:"center", margin:"auto", padding: "1rem",color:"white"}}>NetflixClone - Created by Skyler Conley 2023</h6>
+        </section>
+    )
+}
