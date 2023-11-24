c0e3b8302878bfb209f20b3775716542dc81b967
Vonglory176 <SkylerGConley@gmail.com>
2023-11-23 17:22:00 -0800

    Changed button to Route to login page. Needs work

--- src/components/EmailSignUp.jsx
+++ src/components/EmailSignUp.jsx
@@ -1,19 +1,22 @@
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Button, Form } from "react-bootstrap";
 // import RightArrow from "../assets/images/RightArrow.svg"
 import RightArrowSVG from "../assets/images/rightArrow";
+
+//https://stackoverflow.com/questions/72984395/react-js-how-to-redirect-on-form-submit
 
 export default function EmailSignUp() {
     return (
     <div className="emailSignUp-div">
         <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
-        <Form data-bs-theme="dark">
+        <Form data-bs-theme="dark" onSubmit={(e) => {e.preventDefault(); window.location.href='NetflixClone/Login'}}>
             <Form.Group className="inputWrapper" controlId="EmailSignUp">
                 {/* <Form.Label>Email address</Form.Label> */}
-                <Form.Control type="email" required/> {/*placeholder="Email address"*/}
+                <Form.Control type="email" required disabled/>
             </Form.Group>
             <Button type="submit" size="lg">
                 Get Started
-                {/* <img src={RightArrow}/> */}
                 <RightArrowSVG/>
             </Button>
         </Form>
