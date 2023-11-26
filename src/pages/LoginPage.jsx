34255293fcbcc62672e0d4fb3621bb5373546ca3
Vonglory176 <SkylerGConley@gmail.com>
2023-11-26 12:40:45 -0800

    Page is mostly complete, just needs CSS polish

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -8,32 +8,36 @@ export default function Login() {
     return (
         <div id="loginPage-div">
             <header>
-                <Link to="/NetflixClone">
-                    <img src={NetflixClone}/>
+                <Link to="/">
+                    <img src={NetflixClone} alt="NetflixClone Logo"/>
                 </Link>
             </header>
 
             <section id="login-section">
                 <div className="loginWrapper-div">
                     <h1>Sign In</h1>
-                    <Form data-bs-theme="dark" onSubmit={(e) => {e.preventDefault(); window.location.href='/'}}>
-                        <Form.Group className="inputWrapper" controlId="SignUp">
-                            {/* <Form.Label>Email address</Form.Label> */}
-                            <Form.Control type="email" placeholder='Email or phone number (Disabled)' required disabled/>
-                            <Form.Control type="password" placeholder='Password (Disabled)' required disabled/>
+
+                    <Form data-bs-theme="dark"> {/* onSubmit={(e) => {e.preventDefault(); window.location.href='/NetflixClone'}} */}
+                        <Form.Group className="inputWrapper" controlId="EmailSignIn">
+                            <Form.Control type="email" required disabled/>
                         </Form.Group>
-                        <Button type="submit" size="lg">
-                            Sign In
-                        </Button>
+
+                        <Form.Group className="inputWrapper" controlId="PasswordSignIn">
+                            <Form.Control type="password" required disabled/>
+                        </Form.Group>
+
+                        <Link to="/">
+                            <Button type="submit" size="lg">Sign In</Button>
+                        </Link>
                         <div id="loginRememberMe-div">
                             <div>
                                 <input type='checkbox' name="rememberMe-input"/>
                                 <label htmlFor="rememberMe-input">Remember Me</label>
                             </div>
-                            <a>Need help?</a>
+                            <a href="#">Need help?</a>
                         </div>
                     </Form>
-                    <p>New to NetflixClone? <a>Sign Up Now</a>.</p>
+                    <p>New to NetflixClone? <a href="#">Sign Up Now</a>.</p>
                     <p>Keep in mind that this is a portfolio project and not actually Netflix. Please do not enter any personal information. <a href="https://github.com/Vonglory176/NetflixClone" target="_blank">View The Github Repo.</a></p>
 
                     {/* GOOGLE CAPTCHA LOGIN/NOTICE WOULD GO HERE */}
