4b0860fdf1b7d3c707a008a47e6c8a77e0d61984
Vonglory176 <SkylerGConley@gmail.com>
2023-11-25 16:09:57 -0800

    More general work, VERY messy but core is there

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,5 @@
 import Footer from '../components/Footer.jsx'
-import NetflixClone from "../assets/images/Netflix_Clone_Logo_PMS.png"
+import NetflixClone from "../assets/images/Netflix_Clone_Logo_PMS.svg"
 import CopyrightBanner from '../components/CopyrightBanner.jsx'
 import { Link } from 'react-router-dom'
 import { Form, Button } from 'react-bootstrap'
@@ -14,27 +14,35 @@ export default function Login() {
             </header>
 
             <section id="login-section">
-                <h1>Sign In</h1>
-                <Form data-bs-theme="dark" onSubmit={(e) => {e.preventDefault(); window.location.href='/'}}>
-                    <Form.Group className="inputWrapper" controlId="EmailSignUp">
-                        {/* <Form.Label>Email address</Form.Label> */}
-                        <Form.Control type="email" placeholder='Email or phone number' required disabled/>
-                        <Form.Control type="password" placeholder='Password' required disabled/>
-                    </Form.Group>
-                    <Button type="submit" size="lg">
-                        Sign In
-                    </Button>
-                    <div>
-                        <input type='checkbox' name="rememberMe-input"/>
-                        <label for="rememberMe-input">Remember Me</label>
-                        <a>Need help?</a>
-                    </div>
+                <div className="loginWrapper-div">
+                    <h1>Sign In</h1>
+                    <Form data-bs-theme="dark" onSubmit={(e) => {e.preventDefault(); window.location.href='/'}}>
+                        <Form.Group className="inputWrapper" controlId="SignUp">
+                            {/* <Form.Label>Email address</Form.Label> */}
+                            <Form.Control type="email" placeholder='Email or phone number (Disabled)' required disabled/>
+                            <Form.Control type="password" placeholder='Password (Disabled)' required disabled/>
+                        </Form.Group>
+                        <Button type="submit" size="lg">
+                            Sign In
+                        </Button>
+                        <div id="loginRememberMe-div">
+                            <div>
+                                <input type='checkbox' name="rememberMe-input"/>
+                                <label htmlFor="rememberMe-input">Remember Me</label>
+                            </div>
+                            <a>Need help?</a>
+                        </div>
+                    </Form>
                     <p>New to NetflixClone? <a>Sign Up Now</a>.</p>
-                    <span>Keep in mind that this is a portfolio project and not actually Netflix. Please do not enter any personal information. <a href="https://github.com/Vonglory176/NetflixClone" target="_blank">View The Github Repo</a></span>
-                </Form>
-            </section>
+                    <p>Keep in mind that this is a portfolio project and not actually Netflix. Please do not enter any personal information. <a href="https://github.com/Vonglory176/NetflixClone" target="_blank">View The Github Repo.</a></p>
 
-            <Footer/>
+                    {/* GOOGLE CAPTCHA LOGIN/NOTICE WOULD GO HERE */}
+
+                </div>
+            </section>
+            <div id="footerWrapper-div">
+                <Footer/>
+            </div>
             <CopyrightBanner/>
         </div>
     )
